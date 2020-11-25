8168437e0732675cec7042a78cec5cdbd0f93a67
kento yoshidu <kento_yoshidu@yahoo.co.jp>
2020-11-25 22:02:17 +0900

    #13 Create footer.js
    footerをコンポーネント化

--- src/pages/index.js
+++ src/pages/index.js
@@ -4,6 +4,7 @@ import { Link, graphql } from "gatsby"
 import Bio from "../components/bio"
 import Layout from "../components/layout"
 import SEO from "../components/seo"
+import Footer from "../components/footer"
 import "../scss/style.scss"
 
 const BlogIndex = ({ data, location }) => {
@@ -85,6 +86,7 @@ const BlogIndex = ({ data, location }) => {
           )
         })}
       </ol>
+      <Footer />
     </div>
     //</Layout>
   )
