701a68596afb6a1b4d1e4e337d221a34de74fae4
kento-yoshidu <kento yoshizu>
2021-01-19 20:46:10 +0900

    #78 Setting Google-Analytics

--- gatsby-config.js
+++ gatsby-config.js
@@ -59,7 +59,7 @@ module.exports = {
     {
       resolve: `gatsby-plugin-google-analytics`,
       options: {
-        //trackingId: `ADD YOUR TRACKING ID HERE`,
+        trackingId: `UA-177277170-1`,
       },
     },
     `gatsby-plugin-feed`,
