b6710187eb9694f5751471b0e5327dff7c7a2794
kento-yoshidu <kento yoshizu>
2021-02-25 21:13:36 +0900

    WIP #13 Update footer.js

--- src/components/footer.js
+++ src/components/footer.js
@@ -1,6 +1,51 @@
 import React from "react"
+import { Link, useStaticQuery, graphql, useScrollRestoration } from "gatsby"
 
-export default () => (
-  <footer className="footer">
-  </footer>
-)
+
+const Footer = ({}) => {
+
+  const { site } = useStaticQuery(
+    graphql`
+      query {
+        site {
+          siteMetadata {
+            title
+            subTitle
+            siteUrl
+            author {
+              name
+            }
+          }
+        }
+      }
+    `
+  )
+
+  return (
+    <footer className="footer">
+      <hgroup>
+        <h1>
+          <Link to={ site.siteMetadata.siteUrl }>
+            { site.siteMetadata.title}
+          </Link>
+        </h1>
+        <h2>
+          { site.siteMetadata.subTitle }
+        </h2>
+      </hgroup>
+
+      <p>jgapjrpajg</p>
+      <p>jgapjrpajg</p>
+      <p>jgapjrpajg</p>
+      <p>jgapjrpajg</p>
+      <p>jgapjrpajg</p>
+      <p>jgapjrpajg</p>
+      <p>jgapjrpajg</p>
+      <p>jgapjrpajg</p>
+      <p>jgapjrpajg</p>
+      <p>jgapjrpajg</p>
+    </footer>
+  )
+}
+
+export default Footer
