c899d4046aa65f824b6bf83d064e1d3b4348b77d
kento <kento@toriwatari.bird>
2021-11-07 11:33:54 +0900

    🌠 #262 SCSSのインストール

--- src/components/layout.tsx
+++ src/components/layout.tsx
@@ -10,7 +10,7 @@ import PropTypes from "prop-types"
 import { useStaticQuery, graphql } from "gatsby"
 
 import Header from "./header"
-import "./layout.css"
+import "./layout.scss"
 
 const Layout = ({ children }) => {
   const data = useStaticQuery<GatsbyTypes.SiteTitleQueryQuery>(graphql`
@@ -26,18 +26,9 @@ const Layout = ({ children }) => {
   return (
     <>
       <Header siteTitle={data.site.siteMetadata?.title || `Title`} />
-      <div
-        style={{
-          margin: `0 auto`,
-          maxWidth: 960,
-          padding: `0 1.0875rem 1.45rem`,
-        }}
-      >
+      <div>
         <main>{children}</main>
         <footer
-          style={{
-            marginTop: `2rem`,
-          }}
         >
           © {new Date().getFullYear()}, Built with
           {` `}
