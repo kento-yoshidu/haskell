51d885e8afa4e46a7ca7d3c1d513c18558412673
kento-yoshidu <kento_yoshidu@yahoo.co.jp>
2026-02-01 12:32:06 +0900

    😧 ABC443 A

---
+++ AtCoder/abc4xx/abc443/a.hs
@@ -0,0 +1,16 @@
+-- https://atcoder.jp/contests/abc443/tasks/abc443_a
+
+fn :: String -> String
+fn s =
+    s ++ "s"
+
+main :: IO ()
+main = do
+    putStrLn (fn "http")
+    -- https
+
+    putStrLn (fn "append")
+    -- appends
+
+    putStrLn (fn "beginner")
+    -- beginners
